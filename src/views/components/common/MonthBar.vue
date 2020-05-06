1c61d7db693b65634f579af221261ce3baab017f
Lester Pérez <prz.lester.dev@gmail.com>
2020-05-06 02:12:22 +0200

    Modified community page

---
+++ src/views/components/common/MonthBar.vue
@@ -0,0 +1,16 @@
+<template>
+  $END$
+</template>
+<script lang=ts>
+  import Vue from 'vue'
+  import Component from 'vue-class-component'
+
+  @Component({
+    name: "MonthBar"
+  })
+  export default class MonthBar extends Vue {
+  }
+</script>
+<style scoped>
+
+</style>
